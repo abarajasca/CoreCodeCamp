3872b8f88bc7dec45eb094ee983cde79410c899a
Adrian Barajas <a_barajas_c@hotmail.com>
2020-10-28 16:58:39 -0600

    prepare project and configure SQLite

---
+++ Startup.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreCodeCamp.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace CoreCodeCamp
+{
+  public class Startup
+  {
+
+        private readonly IConfiguration _configuration;
+        public Startup(IConfiguration configuration)
+        {
+                _configuration = configuration;
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<CampContext>();
+            services.AddScoped<ICampRepository, CampRepository>();
+
+          services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+          if (env.IsDevelopment())
+          {
+            app.UseDeveloperExceptionPage();
+          }
+
+          app.UseRouting();
+
+          app.UseAuthentication();
+          app.UseAuthorization();
+
+          app.UseEndpoints(cfg =>
+          {
+            cfg.MapControllers();
+          });
+        }
+      }
+}
